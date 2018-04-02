1f6914b57361ce42d2bdee02b8fa3377a038aa80
Nikolay Dikhtyar <nikolay.dikhtyar@onix-systems.com>
2018-04-03 00:21:59 +0300

    Added htaccess

--- application/index.php
+++ application/index.php
@@ -1,4 +1,4 @@
 <?php
     print "<h3>Welcome to environment demo!</h3><br>";
-    print "Current environment is: " + getenv("ENVIRONMENT");
+    print "Current environment is: " . getenv("ENVIRONMENT");
 ?>
